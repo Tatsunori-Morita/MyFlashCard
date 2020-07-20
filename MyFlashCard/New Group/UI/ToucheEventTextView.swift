11311f1bdb450fe9c28f7de8794d9f2c81e0bd13
Tatsunori Morita <tatsunori.morita@gmail.com>
2020-07-20 14:24:03 +0900

    カード一覧のlabelをtextviewに変更

--- MyFlashCard/New Group/UI/ToucheEventTextView.swift
+++ MyFlashCard/New Group/UI/ToucheEventTextView.swift
@@ -24,4 +24,36 @@ class ToucheEventTextView: UITextView {
         topCorrection = max(0, topCorrection)
         contentInset = UIEdgeInsets(top: topCorrection, left: 0, bottom: 0, right: 0)
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let next = next {
+            next.touchesBegan(touches , with: event)
+        } else {
+            super.touchesBegan(touches , with: event)
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let next = next {
+            next.touchesEnded(touches , with: event)
+        } else {
+            super.touchesEnded(touches , with: event)
+        }
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let next = next {
+            next.touchesCancelled(touches, with: event)
+        } else {
+            super.touchesCancelled(touches, with: event)
+        }
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let next = next {
+            next.touchesMoved(touches, with: event)
+        } else {
+            super.touchesMoved(touches, with: event)
+        }
+    }
 }
